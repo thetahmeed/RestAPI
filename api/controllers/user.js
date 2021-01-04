cb63fd5b733b934ce400b00777225c4b2b6bb229
thetahmeed <tahmeed.com.bd@gmail.com>
2021-01-04 17:22:52 +0600

    Update user.js

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -19,22 +19,16 @@ const singUpUser = (req, res, next) => {
                 error: err
             })
         }else{
-            res.json({
-                hash
+            const newUser = new User({
+                email: req.body.email,
+                password: hash
             })
+        
+            newUser.save()
+                .then(data => res.status(201).send('<h1>User created</h1>'))
+                .catch(err => console.log(err))
         }
     })
-
-    /*
-    const newUser = new User({
-        email: req.body.email,
-        password: ""
-    })
-
-    newUser.save()
-        .then(data => res.send('<h1>User created</h1>'))
-        .catch(err => console.log(err))
-    */
 }
 
 const singInUser = (req, res, next) => {
@@ -45,7 +39,9 @@ const allUser = (req, res, next) => {
     
     User.find()
         .then(data => {
-            console.log(data)
+            res.status(200).json({
+                data
+            })
         })
         .catch(err => console.log(err))
    
