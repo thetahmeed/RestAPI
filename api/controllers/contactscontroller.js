ae7e26f0a921ab2af3a66935b79eaf0d62aa42d0
thetahmeed <tahmeed.com.bd@gmail.com>
2021-01-04 12:31:53 +0600

    separated all controllers

--- api/controllers/contactscontroller.js
+++ api/controllers/contactscontroller.js
@@ -4,3 +4,52 @@
 
 
 
+// Importing model
+const mContactsModel = require('../models/contactmodel')
+
+const getAllContacts = (req, res, next) => {
+    mContactsModel.find()
+                .then(data => res.status(200).json({data}))
+                .catch(err => {
+                    res.status(500).json({
+                        message: "Error:",
+                        error: err
+                    })
+                }) 
+}
+
+const postNewContact = (req, res, next) => {
+    const contactmodel = new mContactsModel({
+        // we can check validity here
+        name: req.body.name,                                            // extracting data from the body
+        phone: req.body.phone,
+        email: req.body.email
+    })
+
+    contactmodel.save()
+            .then(data => res.status('201').json(data))
+            .catch(err => {
+                res.status(500).json({
+                    message: "Error:",
+                    error: err
+                })
+            })
+}
+
+const getSingleContactById = (req, res, next) => {
+
+}
+
+const updateContactById = (req, res, next) => {
+
+}
+
+const deleteContactById = (req, res, next) => {
+
+}
+
+
+module.exports = {
+    getAllContacts,
+    postNewContact
+}
--- api/routes/contactsroute.js
+++ api/routes/contactsroute.js
@@ -7,53 +7,21 @@
 const mExpress = require('express')
 const mContactsRoute = mExpress.Router()
 
-// Importing model
-const mContactsModel = require('../models/contactmodel')
+
+
+// Importing controller
+const {getAllContacts, postNewContact} = require('../controllers/contactscontroller')
 
 // Getting all post
-mContactsRoute.get('/', (req, res, next) => {
+mContactsRoute.get('/', getAllContacts)
 
-    mContactsModel.find()
-                .then(data => res.status(200).json({data}))
-                .catch(err => {
-                    res.status(500).json({
-                        message: "Error:",
-                        error: err
-                    })
-                }) 
-})
+// Posting a new post
+mContactsRoute.post('/', postNewContact)
 
 // Getting a single post
 mContactsRoute.get('/:id', (req, res, next) => {
 
-    const id = req.params.id
-
-    res.json({
-        message : id
-    })
 })
-
-
-// Posting a new post
-mContactsRoute.post('/', (req, res, next) => {
-
-    const contactmodel = new mContactsModel({
-        // we can check validity here
-        name: req.body.name,                                            // extracting data from the body
-        phone: req.body.phone,
-        email: req.body.email
-    })
-
-    contactmodel.save()
-            .then(data => res.status('201').json(data))
-            .catch(err => {
-                res.status(500).json({
-                    message: "Error:",
-                    error: err
-                })
-            })
-})
-
 
 // Updating a post
 mContactsRoute.put('/:id', (req, res, next) => {
