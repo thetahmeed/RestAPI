02d1ed1a1573b9193be2c84971eb25518a888cc4
thetahmeed <tahmeed.com.bd@gmail.com>
2021-01-04 17:12:43 +0600

    created a hash (incode3d version of the given passwosd)

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -4,19 +4,37 @@
 
 
 
+//
+const mBcrypt = require('bcrypt')
+
 // getting user model
 const User = require('../models/User')
 
 const singUpUser = (req, res, next) => {
 
+    mBcrypt.hash(req.body.password, 10, (err, hash) => {
+        if(err){
+            res.json({
+                message: "Error:",
+                error: err
+            })
+        }else{
+            res.json({
+                hash
+            })
+        }
+    })
+
+    /*
     const newUser = new User({
         email: req.body.email,
-        password: req.body.password
+        password: ""
     })
 
     newUser.save()
         .then(data => res.send('<h1>User created</h1>'))
         .catch(err => console.log(err))
+    */
 }
 
 const singInUser = (req, res, next) => {
