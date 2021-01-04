7ab6670f3a5346b89cd90bda1ea0fc64d0c8430d
thetahmeed <tahmeed.com.bd@gmail.com>
2021-01-04 17:05:18 +0600

    install bcrypt to dCript our password
    npm i bcrypt

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -4,8 +4,19 @@
 
 
 
+// getting user model
+const User = require('../models/User')
+
 const singUpUser = (req, res, next) => {
-    res.send('Hi new user')
+
+    const newUser = new User({
+        email: req.body.email,
+        password: req.body.password
+    })
+
+    newUser.save()
+        .then(data => res.send('<h1>User created</h1>'))
+        .catch(err => console.log(err))
 }
 
 const singInUser = (req, res, next) => {
@@ -13,7 +24,13 @@ const singInUser = (req, res, next) => {
 }
 
 const allUser = (req, res, next) => {
-    res.send('Hi all user')
+    
+    User.find()
+        .then(data => {
+            console.log(data)
+        })
+        .catch(err => console.log(err))
+   
 }
 
 module.exports = {
