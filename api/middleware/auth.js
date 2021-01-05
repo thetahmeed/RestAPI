ff86fc006a29db8ec7f1b2784dff5692c99adc6e
thetahmeed <tahmeed.com.bd@gmail.com>
2021-01-05 12:03:00 +0600

    FINISH
    We can use MULTER to upload files.

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -8,15 +8,14 @@ const mJsonWebToken = require('jsonwebtoken')
 
 const auth = (req, res, next) => {
     try{
-        console.log('I am here 1')
+    
         const mToken = req.headers.authorization.split(' ')[1]
-        console.log('I am here 1')
         const mDecode = mJsonWebToken.verify(mToken, 'SECRET')
-        console.log('I am here 1')
+        
         // we can find out the user by the url/route
         req.user = mDecode
         next()
-        console.log('I am here 1')
+        
     }catch(err){
         res.json({
             message: 'Authentication failed'
